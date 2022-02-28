27d1d71706e2c1c3a93b7e7500af565b0339c836
Darren Santos <65102731+dmcsvoices@users.noreply.github.com>
2022-02-27 20:49:04 -0800

    class properties and fixed the balance bug

--- src/App.js
+++ src/App.js
@@ -6,54 +6,56 @@ import AccountBalance from './components/AccountBalance/AccountBalance'
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      balance: 10000,
-      showBalanceEnabled: false,
-      coinData: [
-        {
-          name: 'Bitcoin',
-          ticker: 'BTC',
-          balance: 0.005,
-          price: 34000.00
-        },
-        {
-          name: 'Ethereum',
-          ticker: 'ETH',
-          balance: 0.5,
-          price: 3200.00
-        },
-        {
-          name: 'Cardano',
-          ticker: 'ADA',
-          balance: 3500,
-          price: 0.90
-        },
-        {
-          name: 'Solana',
-          ticker: 'SOL',
-          balance: 2455,
-          price: 27.00
-        }
-      ]
-    }
-    this.handleRefresh = this.handleRefresh.bind(this);
-    this.handleBalanceEnable = this.handleBalanceEnable.bind(this);
-  }
-  handleRefresh(valueChangeTicker){
-    const newCoinData = this.state.coinData.map( function({ticker, name, price}) {
-      let newPrice = price;
-      if (valueChangeTicker === ticker ) {
-        const randomPercentage = 0.995 + Math.random() * 0.01;
-        newPrice =  newPrice * randomPercentage;
+  
+  state = {
+    balance: 10000,
+    showBalanceEnabled: false,
+    coinData: [
+      {
+        name: 'Bitcoin',
+        ticker: 'BTC',
+        balance: 0.005,
+        price: 34000.00
+      },
+      {
+        name: 'Ethereum',
+        ticker: 'ETH',
+        balance: 0.5,
+        price: 3200.00
+      },
+      {
+        name: 'Cardano',
+        ticker: 'ADA',
+        balance: 3500,
+        price: 0.90
+      },
+      {
+        name: 'Solana',
+        ticker: 'SOL',
+        balance: 2455,
+        price: 27.00
       }
-      return {ticker, name, price: newPrice}
+    ]
+  }
+  
+
+  //componentDidMount = () => {}
+  
+  
+  handleRefresh = (valueChangeTicker) => {
+    const newCoinData = this.state.coinData.map( function(values) {
+      let newValues = { ...values };
+      if (valueChangeTicker === newValues.ticker ) {
+        const randomPercentage = 0.995 + Math.random() * 0.01;
+        newValues.price *=   randomPercentage;
+      }
+      return newValues;
+     
     });
     this.setState({ coinData: newCoinData})
   }
 
-  handleBalanceEnable(valueEnabled){
+  handleBalanceEnable = (valueEnabled) => {
     this.setState({ showBalanceEnabled: !valueEnabled})
   }
 
--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -23,25 +23,15 @@ const Button = styled.button`
 `;
 
 export default class Coin extends Component {
-    constructor(props){
-        super(props);
-        this.handleClick = this.handleClick.bind(this);
-    }
+
     
-    handleClick(event) {
+    handleClick = (event) => {
         //prevent default action of submittin gthe form
         event.preventDefault();
 
         
         this.props.handleRefresh(this.props.ticker);
-/*
-        const randomPercentage = 0.995 + Math.random() * 0.01;
-        this.setState( function(oldState){
-            return {
-                price: oldState.price * randomPercentage
-            };
-        });
-*/
+
     }
     
     render() {
