9ba1d967e4c255a8171b711d898102af1cee7549
Darren Santos <65102731+dmcsvoices@users.noreply.github.com>
2022-03-02 05:56:45 -0800

    added useEffect hook in App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import CoinList from './components/CoinList/CoinList';
 import Header from './components/Header/Header';
@@ -8,45 +8,17 @@ import axios from 'axios';
 const COIN_COUNT = 10;
 const formatPrice = price =>  parseFloat(Number(price).toFixed(4));
 
-class App extends React.Component {
+function App(props) {
   
-  state = {
-    balance: 10000,
-    showBalanceEnabled: false,
-    coinData: [
-     /* {
-        name: 'Bitcoin',
-        ticker: 'BTC',
-        balance: 0.005,
-        price: 34000.00
-      },
-      {
-        name: 'Ethereum',
-        ticker: 'ETH',
-        balance: 0.5,
-        price: 3200.00
-      },
-      {
-        name: 'Cardano',
-        ticker: 'ADA',
-        balance: 3500,
-        price: 0.90
-      },
-      {
-        name: 'Solana',
-        ticker: 'SOL',
-        balance: 2455,
-        price: 27.00
-      }*/
-    ]
-  }
-   
 
-  componentDidMount = async () => {
-    const response = await axios.get('https://api.coinpaprika.com/v1/coins');
-      
+   
+  const [balance, setbalance] = useState(10000);
+  const [showBalanceEnabled, setshowBalanceEnabled] = useState(true);
+  const [coinData, setCoinData] = useState([]);
+
+  const componentDidMount = async () => {
+    const response = await axios.get('https://api.coinpaprika.com/v1/coins');   
     const coinIds = response.data.slice(0, COIN_COUNT).map(coin => coin.id);
-   //console.log("componentsDidMount, coinIds : ", coinIds);
     //retrieve the prices here
     const tickerUrl ='https://api.coinpaprika.com/v1/tickers/';
     const promises = coinIds.map(id => axios.get(tickerUrl + id));
@@ -61,20 +33,28 @@ class App extends React.Component {
         price: formatPrice(coin.quotes.USD.price),
       };
     })
-  
-
-    this.setState({ coinData: coinPriceData });
- 
-  
+    setCoinData(coinPriceData);
   }
   
+
+  useEffect(function(){
+    //only load data if we need it
+    if (coinData.length === 0) {
+      //component did mount
+      componentDidMount();
+    } 
+
+
+  });
+
+
   
-  handleRefresh = async (valueChangeId) => {
+  const handleRefresh = async (valueChangeId) => {
     const tickerUrl =`https://api.coinpaprika.com/v1/tickers/${valueChangeId}`;
     const response = await axios.get(tickerUrl);
     
     const newPrice = formatPrice(response.data.quotes.USD.price);
-    const newCoinData = this.state.coinData.map( function(values) {
+    const newCoinData = coinData.map( function(values) {
       let newValues = { ...values };
       if (valueChangeId === newValues.key ) {
         newValues.price = newPrice;
@@ -82,19 +62,20 @@ class App extends React.Component {
       return newValues;
     
     });
-    this.setState({ coinData: newCoinData})
+
+    setCoinData(newCoinData);
   }
 
-  handleBalanceEnable = (valueEnabled) => {
-    this.setState({ showBalanceEnabled: !valueEnabled})
+  const handleBalanceEnable = (valueEnabled) => {
+    setshowBalanceEnabled(valueEnabled =>!valueEnabled);
   }
 
-  render() {
+
     return (
       <div className="App">
-        <Header showBalance={this.state.showBalanceEnabled} />
-        <AccountBalance amount={this.state.balance} showBalance={this.state.showBalanceEnabled} handleBalanceEnable={this.handleBalanceEnable} />
-        <CoinList coinData={this.state.coinData} showBalance={this.state.showBalanceEnabled} handleRefresh={this.handleRefresh} />
+        <Header showBalance={showBalanceEnabled} />
+        <AccountBalance amount={balance} showBalance={showBalanceEnabled} handleBalanceEnable={handleBalanceEnable} />
+        <CoinList coinData={coinData} showBalance={showBalanceEnabled} handleRefresh={handleRefresh} />
       
       <div>
           <iframe width="396" height="441" src="https://cybermap.kaspersky.com/en/widget/dynamic/dark" frameborder="0"></iframe>
@@ -102,7 +83,7 @@ class App extends React.Component {
   
       </div>
     );
-  }
+  
 
  
 }
